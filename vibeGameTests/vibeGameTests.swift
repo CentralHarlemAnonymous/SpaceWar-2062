d0171b9e93037e52dee7471d4cc56f29ca472e76
CentralHarlemAnonymous <nycstern+github@gmail.com>
2026-01-09 23:10:58 -0500

    Initial Commit

---
+++ vibeGameTests/vibeGameTests.swift
@@ -0,0 +1,17 @@
+//
+//  vibeGameTests.swift
+//  vibeGameTests
+//
+//  Created by Michael Stern on 1/9/26.
+//
+
+import Testing
+@testable import vibeGame
+
+struct vibeGameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
